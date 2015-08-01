0a95d87240631aeccf6826b2328ddbb84ca19d27
alyksia <rtpavlovsk21@gmail.com>
2015-08-01 06:06:05 -0700

    add list saving to file

--- app/src/main/java/com/example/alyksia/myapplication/MainActivity.java
+++ app/src/main/java/com/example/alyksia/myapplication/MainActivity.java
@@ -10,6 +10,10 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lvItems = (ListView) findViewById(R.id.lvItems);
-        items = new ArrayList<String>();
+        readItems();
         itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
         items.add("First");
@@ -30,16 +34,45 @@ public class MainActivity extends AppCompatActivity {
         setupListViewListener();
     }
 
+    public void onAddItem(View v){
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+    }
+
     private void setupListViewListener(){
         lvItems.setOnItemLongClickListener(
-                new AdapterView.OnItemLongClickListener(){
+                new AdapterView.OnItemLongClickListener() {
                     @Override
-                    public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id){
+                    public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
                         items.remove(pos);
                         itemsAdapter.notifyDataSetChanged();
+                        writeItems();
                         return true;
                     }
-        });
+                });
+    }
+
+    private void readItems(){
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir,"todo.txt");
+        try{
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        }catch (IOException e){
+            items = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems(){
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir,"todo.txt");
+        try{
+            FileUtils.writeLines(todoFile, items);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -64,10 +97,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onAddItem(View v){
-        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
-        String itemText = etNewItem.getText().toString();
-        itemsAdapter.add(itemText);
-        etNewItem.setText("");
-    }
 }
